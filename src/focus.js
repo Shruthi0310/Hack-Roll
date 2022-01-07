5f53bb5453842ad03ae2d7678d146ecb9cb777fa
Shruthi0310 <e0564271@u.nus.edu>
2022-01-08 04:03:45 +0800

    Update block Ui

--- src/focus.js
+++ src/focus.js
@@ -1,12 +1,39 @@
-//get from home page
-var whitelistWebsites = ["www.youtube.com", "www.google.com"];
+var whitelistWebsites = ["www.youtube.com", "www.facebook.com"];
 
 const getHTML = () => {
     return (
-        `<div style="background-color:rgb(255, 99, 71);">GO BACK TO WORK</div>`
+        `<div class='textbody'>
+        <div class='text'><i>GET BACK TO WORK</i></div>
+        <hr>
+        </div>`
+    
     )
 }
 
+const getStyle = () => {
+    return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
+    body {
+        background: #8dabba;
+        color: #fff;
+        font-family: "Open Sans", sans-serif;
+        max-height: 700px;
+        overflow: hidden;
+    }
+
+    .textbody {
+        text-align: center;
+    }
+
+    .text {
+        font-size: 100px;
+        margin-top: 20%
+    }`
+
+}
+
 if (whitelistWebsites.includes(window.location.hostname)) {
+    document.head.innerHTML = getStyle();
         document.body.innerHTML = getHTML();
 }
+
+
