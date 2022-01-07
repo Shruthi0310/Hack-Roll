6e07f4a182c33e0dcc96c6aa7e3c7ce5c0030d27
Shruthi0310 <e0564271@u.nus.edu>
2022-01-07 18:03:16 +0800

    Update block feature

--- src/focus.js
+++ src/focus.js
@@ -1,3 +1,12 @@
-if (window.location.hostname === "www.youtube.com") {
-    alert("hahahha");
+//get from home page
+var whitelistWebsites = ["www.youtube.com", "www.google.com"];
+
+const getHTML = () => {
+    return (
+        `<div style="background-color:rgb(255, 99, 71);">GO BACK TO WORK</div>`
+    )
+}
+
+if (whitelistWebsites.includes(window.location.hostname)) {
+        document.body.innerHTML = getHTML();
 }
